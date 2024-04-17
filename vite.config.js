8798f589381371ab72728db403124a9819450741
marve10s <igrimanigroman@gmail.com>
2024-04-17 20:59:33 +0300

    MULTIPAGE IS WORKING AAAAAAA

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        gallery: resolve(__dirname, "gallery/gallery.html"),
+        nested: resolve(__dirname, "gallery/gallery.html"),
       },
     },
   },
